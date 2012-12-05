2a9e36cf2e24466d4b7d9a7409012679b38e0739
Takashi Matsuda <mazda.takasky@gmail.com>
2012-12-05 11:02:15 +0900

    テストコード

---
+++ src/clustering/HamerlyClustering.java
@@ -0,0 +1,20 @@
+package clustering;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Fast k-means clustering by Hamer.
+ * Hamerの方法によるクラスタリングの実装
+ * @author takashi
+ *
+ */
+public class HamerlyClustering implements Clustering {
+
+	@Override
+	public List<double[][]> Ksplit(int k, LinkedList<double[]> data) {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+}
---
+++ src/clustering/LloydClusteringTest.java
@@ -0,0 +1,21 @@
+package clustering;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LloydClusteringTest {
+
+	@Test
+	public void testKsplit() {
+		// 乱数発生器の使い方を確認して書く
+		
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testLloydClustering() {
+		fail("まだ実装されていません");
+	}
+
+}
