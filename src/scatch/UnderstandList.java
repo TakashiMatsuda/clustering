57621a475b6cebb71663e349f22f1557fc11c557
Takashi Matsuda <mazda.takasky@gmail.com>
2012-12-06 11:55:17 +0900

    リストの挙動を確かめています

---
+++ src/scatch/UnderstandList.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package scatch;
+
+/**
+ * @author takashi
+ *	リストの挙動を確かめるためのクラス
+ */
+public class UnderstandList {
+	
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args){
+		
+	}
+	
+	
+}
