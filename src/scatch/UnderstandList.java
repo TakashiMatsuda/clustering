06679c82bd904dd5fe0ab34e13a7a7fb601693fe
Takashi Matsuda <mazda.takasky@gmail.com>
2012-12-06 11:58:59 +0900

    リストの挙動を確かめています

--- src/scatch/UnderstandList.java
+++ src/scatch/UnderstandList.java
@@ -3,6 +3,8 @@
  */
 package scatch;
 
+import java.util.LinkedList;
+
 /**
  * @author takashi
  *	リストの挙動を確かめるためのクラス
@@ -14,6 +16,22 @@ public class UnderstandList {
 	 * @param args
 	 */
 	public static void main(String[] args){
+		/*
+		 * 適当にリストを作成
+		 * 
+		 * 
+		 */
+		
+		
+		LinkedList<ArrayList<Integer>> tester = new LinkedList<ArrayList<Integer>>();
+		/*
+		 * 順番に整数を格納
+		 */
+		for(int i = 0; i < 5; i++){
+			for(int  j = 0; j < 10; j++){
+				
+			}
+		}
 		
 	}
 	
