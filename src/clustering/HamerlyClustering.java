ae68f15f53dfbf89b712d540a55126215990a54d
Takashi Matsuda <mazda.takasky@gmail.com>
2012-12-09 15:53:39 +0900

    HamerlyClusteringにnormとdistanceを追加

--- src/clustering/HamerlyClustering.java
+++ src/clustering/HamerlyClustering.java
@@ -10,7 +10,48 @@ import java.util.List;
  *
  */
 public class HamerlyClustering implements Clustering{
+	private static final boolean DEBUG = false;
+	
+	private int n;
+	private int d;
+	private int k;
+	private byte[][] indicator;
+	
 
+	/**
+	 * 
+	 * ベクトルのノルム
+	 * @param tmp
+	 * @return absolute value
+	 */
+	private double norm(double[] x) {
+		double sum = 0;
+		for (int i = 0; i < x.length; i++){
+			sum += Math.pow(x[i], 2.0);
+		}
+		return Math.sqrt(sum);
+	}
+	
+	/**
+	 * xとyの次元数が一致していることを確認して下さい!
+	 * 
+	 * @param x
+	 * @param y
+	 * @return Euclid distance between x and y
+	 */
+	private double distance(double[] x, double[] y){
+		double sum = 0;
+		/*
+		 * イテレータを用いて書いてみたいですね
+		 * x, yどちらもに必要なパラメータだから使えないのでは？
+		 */
+		for (int i = 0; i < x.length; i++){
+			sum += Math.pow((x[i] - y[i]), 2.0);
+		}
+		return Math.sqrt(sum);
+	}
+	
+	
 	/**
 	 *  Hamerの方法によるクラスタリングの結果を返します。
 	 * @param k : target clusters number
@@ -19,7 +60,20 @@ public class HamerlyClustering implements Clustering{
 	 */
 	@Override
 	public byte[][] Ksplit(int k, LinkedList<double[]> data) {
-		// TODO 自動生成されたメソッド・スタブ
+		this.n = dataSpace.size();
+		this.d = dataSpace.get(0).length;
+		this.k = khiki;
+		this.indicator = new byte[n][k];
+		
+		
+		/**
+		 * Initiation
+		 * Select initial delegation.
+		 */
+		double[][] delegation  = new double[k][d];
+		for(int i = 0; i < k; i++){
+			delegation[i] = dataSpace.get(i);
+		}
 		
 		
 		
